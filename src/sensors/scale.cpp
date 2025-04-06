b301459e0522dc72bba303cfb3213e2c8b12aaf9
marcilioc <caiomds2912@outlook.com>
2025-04-06 20:25:12 -0300

    feat: Added ADCs configuration to scale

--- src/sensors/scale.cpp
+++ src/sensors/scale.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "scale.h"
+#include <driver/adc.h>
 
 HX711 scale;
 
@@ -11,6 +12,12 @@ void set_scale_zero() {
   }
 
 void setup_scale() {
+    // ADCs configurarion
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_0);
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_0);
+
     // Start scale measurement
     scale.set_scale(CALIBRATION_FACTOR);  
     scale.begin(DOUT, CLK);
